930262f18e10a1703530a9e86cdacad55c2ce875
Arinjain2003 <jainarin416@gmail.com>
2024-07-10 14:41:50 +0530

    added bcryptjs

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,5 +1,5 @@
 const User = require("../models/user_model");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const { use } = require("../router/auth-router");
 
 // HOME PAGE
